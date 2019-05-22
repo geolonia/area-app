6dfde52fe6639fd1a2a4181ac002beb6933fb1b9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-05-23 00:55:26 +0900

    some fix

--- src/app.js
+++ src/app.js
@@ -6,22 +6,30 @@ const areaDome = 46755
 const map = new tilecloud.Map( document.querySelector( '#map' ) )
 
 const clickFeature = event => {
-  map.queryRenderedFeatures( { layers: [ "building" ] } ).forEach( feature => {
+  map.queryRenderedFeatures( { layters: map.getStyle().layers } ).forEach( feature => {
     map.setFeatureState( feature, { hover: false } )
   } )
 
   const features = map.queryRenderedFeatures( event.point )
-  const feature = features[0]
-  feature.layer.paint["fill-color"] = "#ff0000"
-  map.setFeatureState( feature, { hover: true } )
+  for ( let i = 0; i < features.length; i++ ) {
+    const feature = features[ i ]
+    if ( 'fill' === feature.layer.type ) {
+      feature.layer.paint["fill-color"] = "#ff0000"
+      map.setFeatureState( feature, { hover: true } )
 
-  const area = turfArea( feature.geometry ).toFixed( 1 )
-  const asDome = ( area / areaDome ).toFixed( 1 )
+      const area = turfArea( feature.geometry ).toFixed( 1 )
+      const asDome = ( area / areaDome ).toFixed( 4 )
 
-  new mapboxgl.Popup()
-    .setLngLat( event.lngLat )
-    .setHTML( `この建物の面積は ${area}㎡ で、東京ドーム ${asDome} 個分の面積です。` )
-    .addTo( map );
+      console.log(feature)
+
+      new mapboxgl.Popup()
+        .setLngLat( event.lngLat )
+        .setHTML( `この建物の面積は ${area}㎡ で、東京ドーム ${asDome} 個分の面積です。` )
+        .addTo( map );
+
+      break;
+    }
+  }
 }
 
 const mouseEnter = () => {
@@ -33,19 +41,33 @@ const mouseLeave = () => {
 }
 
 map.on( 'load', () => {
-  map.on('click', "building", clickFeature)
-  map.on('mouseenter', "building", mouseEnter)
-  map.on('mouseleave', "building", mouseLeave)
+  map.getStyle().layers.forEach( (item) => {
+    map.on('click', item.id, clickFeature)
+    map.on('mouseenter', item.id, mouseEnter)
+    map.on('mouseleave', item.id, mouseLeave)
 
-  map.setPaintProperty( "building", 'fill-color', ["case",
-    ["boolean", ["feature-state", "hover"], false],
-    "#ff0000",
-    "#dedede"
-  ] );
+    if ( 'fill' === item.type ) {
+      const color = map.getPaintProperty( item.id, 'fill-color' )
+      let opacity = map.getPaintProperty( item.id, 'fill-opacity' )
+      if ( 'undefined' === typeof opacity ) {
+        opacity = 1
+      }
 
-  map.setPaintProperty( "building", 'fill-opacity', ["case",
-    ["boolean", ["feature-state", "hover"], false],
-    0.2,
-    1
-  ] );
+      if ( color ) {
+        map.setPaintProperty( item.id, 'fill-color', ["case",
+          ["boolean", ["feature-state", "hover"], false],
+          "#ff0000",
+          color
+        ] );
+      }
+
+      if ( opacity ) {
+        map.setPaintProperty( item.id, 'fill-opacity', ["case",
+          ["boolean", ["feature-state", "hover"], false],
+          0.2,
+          parseFloat( opacity )
+        ] );
+      }
+    }
+  })
 } )
