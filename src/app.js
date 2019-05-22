85b589649bf6e35d260dccc9493f0d397e693c5d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-05-23 01:43:23 +0900

    add draw button

--- src/app.js
+++ src/app.js
@@ -1,73 +1,41 @@
 import 'babel-polyfill';
 import turfArea from '@turf/area'
+import MapboxDraw from '@mapbox/mapbox-gl-draw'
+import turfCenter from '@turf/center'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import './style.css'
 
 const areaDome = 46755
 
 const map = new tilecloud.Map( document.querySelector( '#map' ) )
 
-const clickFeature = event => {
-  map.queryRenderedFeatures( { layters: map.getStyle().layers } ).forEach( feature => {
-    map.setFeatureState( feature, { hover: false } )
-  } )
-
-  const features = map.queryRenderedFeatures( event.point )
-  for ( let i = 0; i < features.length; i++ ) {
-    const feature = features[ i ]
-    if ( 'fill' === feature.layer.type ) {
-      feature.layer.paint["fill-color"] = "#ff0000"
-      map.setFeatureState( feature, { hover: true } )
-
-      const area = turfArea( feature.geometry ).toFixed( 1 )
-      const asDome = ( area / areaDome ).toFixed( 4 )
-
-      console.log(feature)
-
-      new mapboxgl.Popup()
-        .setLngLat( event.lngLat )
-        .setHTML( `この建物の面積は ${area}㎡ で、東京ドーム ${asDome} 個分の面積です。` )
-        .addTo( map );
-
-      break;
-    }
-  }
-}
-
-const mouseEnter = () => {
-  map.getCanvas().style.cursor = 'pointer'
-}
-
-const mouseLeave = () => {
-  map.getCanvas().style.cursor = ''
-}
+const draw = new MapboxDraw( {
+  boxSelect: false,
+  controls: {
+    point: false,
+    line_string: false,
+    polygon: true,
+    trash: true,
+    combine_features: false,
+    uncombine_features: false,
+  },
+  userProperties: true,
+} )
 
 map.on( 'load', () => {
-  map.getStyle().layers.forEach( (item) => {
-    map.on('click', item.id, clickFeature)
-    map.on('mouseenter', item.id, mouseEnter)
-    map.on('mouseleave', item.id, mouseLeave)
+  map.addControl( draw, 'top-right' );
 
-    if ( 'fill' === item.type ) {
-      const color = map.getPaintProperty( item.id, 'fill-color' )
-      let opacity = map.getPaintProperty( item.id, 'fill-opacity' )
-      if ( 'undefined' === typeof opacity ) {
-        opacity = 1
+  [ 'draw.create', 'draw.update', 'draw.delete' ].forEach( eventType => {
+    map.on( eventType, () => {
+      const geoJson = draw.getAll()
+      const area = turfArea( geoJson ).toFixed( 1 )
+      if ( area ) {
+        const asDome = ( area / areaDome ).toFixed( 4 )
+        document.querySelector( '#message' ).innerText = `面積は ${area}㎡ です。東京ドーム ${asDome}個分の広さです。`
+        document.querySelector( '#message' ).style.display = 'block'
+      } else {
+        document.querySelector( '#message' ).style.display = 'none'
       }
-
-      if ( color ) {
-        map.setPaintProperty( item.id, 'fill-color', ["case",
-          ["boolean", ["feature-state", "hover"], false],
-          "#ff0000",
-          color
-        ] );
-      }
-
-      if ( opacity ) {
-        map.setPaintProperty( item.id, 'fill-opacity', ["case",
-          ["boolean", ["feature-state", "hover"], false],
-          0.2,
-          parseFloat( opacity )
-        ] );
-      }
-    }
-  })
+    } )
+  } )
 } )
